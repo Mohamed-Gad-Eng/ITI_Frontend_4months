32924d92a63de1aad40a78f91ad134b81d4d17c1
Mohamed Gad <mohamed.gad.eng@gmail.com>
2026-03-13 15:12:34 +0200

    iRevert "last day in iti latest update"
    This reverts commit a8a6b8122716c7af6cb001244e5e2b9365dfae2d.

--- Day46 Next JS/Tasks/next-pratice-proj/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-// import HomePage from "./Home/page";
-
-export default function Home() {
-  
-  // return <HomePage />;
-
-  redirect("/home");
-}
--- Day48 PWA/Web Worker Api/anotherScript.js
+++
@@ -1,7 +0,0 @@
-console.log(this)
-console.log('hiiiiiiiiiii')
-onmessage = function(event){
-    console.log(event)
-    var sum = parseInt(event.data[0])+parseInt(event.data[1])
-    this.postMessage([sum])
-}
